649a606a73bbf6fc156de612000ac3ef329424a1
Marko Jekrić <markojerkic266@gmail.com>
2024-02-25 22:41:03 +0100

    sync to file

--- auth/user.go
+++ auth/user.go
@@ -1,6 +1,9 @@
 package auth
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"sync"
 
 	"github.com/labstack/echo/v4"
@@ -16,9 +19,40 @@ type RegisteredUsers struct {
 	users map[string]User
 }
 
+func (r *RegisteredUsers) syncToFile() {
+	// write r.users to byte array
+	users, err := json.Marshal(r.users)
+	if err != nil {
+		fmt.Println("Error marshalling users")
+		panic(err)
+	}
+
+	err = os.WriteFile("/tmp/chat-users", []byte(users), 0777)
+
+	if err != nil {
+		fmt.Println("Error writing users to file")
+		panic(err)
+	}
+}
+
 func newRegisteredUsers() *RegisteredUsers {
+	users, err := os.ReadFile("/tmp/chat-users")
+
+	var usersMap map[string]User
+	if err != nil {
+		fmt.Println("No users file found, creating new")
+		usersMap = make(map[string]User)
+	} else {
+		err = json.Unmarshal(users, &usersMap)
+		fmt.Println("Read users from file", usersMap)
+		if err != nil {
+			fmt.Println("Error unmarshalling users")
+			panic(err)
+		}
+	}
+
 	return &RegisteredUsers{
-		users: make(map[string]User),
+		users: usersMap,
 		lock:  sync.RWMutex{},
 	}
 }
@@ -28,6 +62,7 @@ func (r *RegisteredUsers) Add(user User) {
 	defer r.lock.Unlock()
 
 	r.users[user.ID] = user
+	r.syncToFile()
 }
 
 // Get list of all users which are not the user querying
@@ -37,7 +72,7 @@ func (r *RegisteredUsers) GetAllUsers(c *echo.Context) []User {
 
 	currentUsrer := (*c).Get("user").(User)
 
-	var users = make([]User, len(r.users))
+	var users = make([]User, 0, len(r.users))
 
 	for _, user := range r.users {
 		if user.ID != currentUsrer.ID {
